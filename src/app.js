a29ca4e1d6555ac7281550bdc7ff20c5edaf071f
Tejas Mehra <tejasmehra04@gmail.com>
2024-07-11 23:47:36 +0530

    Github Prod

--- src/app.js
+++ src/app.js
@@ -130,10 +130,10 @@ app.get('*', (req, res) => {
     })
 })
 
-// app.listen(3000, () => {
-//     console.log('server is up on port 3000.')
-// })     
+app.listen(3000, () => {
+    console.log('server is up on port 3000.')
+})     
 
-app.listen(port, () => {
-    console.log('server is up on port ' + port)
-}) 
+// app.listen(port, () => {
+//     console.log('server is up on port ' + port)
+// }) 
